b8809df2389db03f626e4434a062aba825997cea
ankuapat <ankupat31@gmail.com>
2026-04-20 11:59:16 -0400

    All audio files, testing done on keypad

---
+++ include/audio_assets.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stdint.h>
+
+extern const uint8_t kick_mp3[];
+extern const unsigned int kick_mp3_len;
+
+extern const uint8_t snare_mp3[];
+extern const unsigned int snare_mp3_len;
+
+extern const uint8_t hat_mp3[];
+extern const unsigned int hat_mp3_len;
+
+extern const uint8_t snare2_mp3[];
+extern const unsigned int snare2_mp3_len;
---
+++ src/audio_assets.c
@@ -0,0 +1,4 @@
+#include "kick_mp3.h"
+#include "snare_mp3.h"
+#include "hat_mp3.h"
+#include "snare2_mp3.h"
